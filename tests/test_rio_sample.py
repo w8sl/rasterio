0e25c451beaebc577bdb4059d2e5759a6b105465
Sylvester <100048048+w8sl@users.noreply.github.com>
2025-07-30 16:44:34 +0200

    Run failing tests with subprocess

--- tests/test_rio_sample.py
+++ tests/test_rio_sample.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-
+import subprocess
 from rasterio.rio.main import main_group
 
 
@@ -15,14 +15,19 @@ def test_sample_err(runner):
     assert result.exit_code == 1
 
 
-def test_sample_stdin(runner):
-    result = runner.invoke(
-        main_group,
-        ['sample', 'tests/data/RGB.byte.tif'],
-        "[220650.0, 2719200.0]\n[220650.0, 2719200.0]",
-        catch_exceptions=False)
-    assert result.exit_code == 0
-    assert result.output.strip() == '[18, 25, 14]\n[18, 25, 14]'
+def test_sample_stdin_subprocess():
+    input_coords = b"[220650.0, 2719200.0]\n[220650.0, 2719200.0]\n"
+    expected_output = '[18, 25, 14]\n[18, 25, 14]'
+
+    proc = subprocess.run(
+        ["rio", "sample", "tests/data/RGB.byte.tif"],
+        input=input_coords,
+        capture_output=True
+    )
+
+    output = proc.stdout.decode("utf-8").strip()
+    assert proc.returncode == 0
+    assert output == expected_output, f"Got unexpected output: {repr(output)}"
 
 
 def test_sample_arg(runner):
